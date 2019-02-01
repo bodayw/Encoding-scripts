96beb4e490e8865227dad00f57446b5c872ae5c9
bodayw <bodayw@gmail.com>
2019-02-01 13:52:57 -0800

    Update _demux.ps1

--- _demux.ps1
+++ _demux.ps1
@@ -1,35 +1,25 @@
 Set-Location $PSScriptRoot
 
-$title = "uma"
+Get-ChildItem *.m2ts -Name | ForEach-Object {C:\Encoding\eac3to\eac3to.exe $_ -demux}
+
+Get-ChildItem *.dtsma -Name | ForEach-Object {C:\Encoding\eac3to\eac3to.exe $_ "$_.flac"} # encode FLAC
 
 # $eps = "01"
 $eps = (1..12)
 $eps = $eps | % {$_.ToString("00")}
 
-function Demux-Batch ($title, $ep) {
- C:\Encoding\mkvtoolnix\mkvmerge.exe --output "$title-$ep.mkv" -A -S -B -T -M -d 0 "$title-$ep.m2ts"
+function Audio-Batch ($ep) {
+ C:\Encoding\eac3to\eac3to.exe "$ep.m2ts" 2: "$ep.flac" -log=nul # encode FLAC
  
- C:\Encoding\mkvtoolnix\mkvmerge.exe --output "$title-$ep.mks" -A -D -B -T -M -s 3 "$title-$ep.m2ts"
-}
-
-ForEach ($ep in $eps) {Demux-Batch $title $ep}
-
-# $eps = "01"
-$eps = (1..12)
-$eps = $eps | % {$_.ToString("00")}
-
-function Audio-Batch ($title, $ep) {
- C:\Encoding\eac3to\eac3to.exe "$title-$ep.m2ts" 2: "$title-$ep.flac" -log=nul # encode FLAC
- 
- cmd /c "C:\Encoding\eac3to\eac3to.exe $title-$ep.m2ts 3: stdout.wav -log=nul | C:\Encoding\qaac\qaac64.exe -v 192 --ignorelength --adts --no-delay -o $title-$ep-2.aac -" # encode AAC (VCB-Studio)
+ cmd /c "C:\Encoding\eac3to\eac3to.exe $ep.m2ts 3: stdout.wav -log=nul | C:\Encoding\qaac\qaac64.exe -v 192 --ignorelength --adts --no-delay -o $ep-2.aac -" # encode AAC (vcb-s)
  
  $ss = "00:00:00.000"
  $to = "00:23:42.004"
- C:\Encoding\ffmpeg.exe -i "$title-$ep.aac" -ss $ss -to $to -c:a copy "$title-$ep.aac" # trim audio
+ C:\Encoding\ffmpeg.exe -i "$ep.aac" -ss $ss -to $to -c:a copy "$ep.aac" # trim audio
  
- cmd /c "C:\Encoding\ffmpeg.exe -i $title-$ep.mkv -map a:0 -f wav pipe: | C:\Encoding\qaac\qaac64.exe -V 100 --ignorelength --adts --no-delay -o $title-$ep.aac -" # encode AAC (LoliHouse)
+ cmd /c "C:\Encoding\ffmpeg.exe -i $ep.mkv -map a:0 -f wav pipe: | C:\Encoding\qaac\qaac64.exe -V 100 --ignorelength --adts --no-delay -o $ep.aac -" # encode AAC (lh)
 }
 
-ForEach ($ep in $eps) {Audio-Batch $title $ep}
+ForEach ($ep in $eps) {Audio-Batch $ep}
 
 Read-Host
