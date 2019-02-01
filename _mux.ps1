a236f18dcdb1b47fff79ec2e5f5acb4dd78b0526
bodayw <bodayw@gmail.com>
2019-02-01 14:05:05 -0800

    Update _mux.ps1

--- _mux.ps1
+++ _mux.ps1
@@ -1,21 +1,19 @@
 Set-Location $PSScriptRoot
 
-$title = "yagate"
-
 # $eps = "01"
 $eps = (1..12)
 $eps = $eps | % {$_.ToString("00")}
 
-function Mux-Batch ($title, $ep) {
+function Mux-Batch ($ep) {
  $filename = "[Airota&Nekomoe kissaten&LoliHouse] Yagate Kimi ni Naru - $ep [WebRip 1080p HEVC-yuv420p10 AAC ASSx2]"
  
- C:\Encoding\AssFontSubset.exe $PSScriptRoot\$title-$ep-sc.ass $PSScriptRoot\$title-$ep-tc.ass | Out-Null
+ C:\Encoding\AssFontSubset.exe $PSScriptRoot\$ep-sc.ass $PSScriptRoot\$ep-tc.ass | Out-Null
  
  $merge_param = "--output", "$filename.mkv", `
- "--language", "0:und", "--default-track", "0:yes", "$title-$ep.hevc", `
- "--language", "0:jpn", "--default-track", "0:yes", "$title-$ep.aac", `
- "--language", "0:chi", "--default-track", "0:yes", "--track-name", "0:SC", "$PSScriptRoot\output\$title-$ep-sc.ass", `
- "--language", "0:chi", "--default-track", "0:no", "--track-name", "0:TC", "$PSScriptRoot\output\$title-$ep-tc.ass"
+ "--language", "0:und", "--default-track", "0:yes", "$ep.hevc", `
+ "--language", "0:jpn", "--default-track", "0:yes", "$ep.aac", `
+ "--language", "0:chi", "--default-track", "0:yes", "--track-name", "0:SC", "$PSScriptRoot\output\$ep-sc.ass", `
+ "--language", "0:chi", "--default-track", "0:no", "--track-name", "0:TC", "$PSScriptRoot\output\$ep-tc.ass"
  
  Get-ChildItem -Path output -Exclude *.ass -Name | ForEach-Object {$merge_param += "--attach-file", "$PSScriptRoot\output\$_"}
  
@@ -26,6 +24,6 @@ function Mux-Batch ($title, $ep) {
  Remove-Item options-file.json
 }
 
-ForEach ($ep in $eps) {Mux-Batch $title $ep}
+ForEach ($ep in $eps) {Mux-Batch $ep}
 
 Read-Host
